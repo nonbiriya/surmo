522bdc3e78b6e68ce44ad769c5d1699b367464f1
nonbiriya <>
2021-11-29 00:19:51 +0900

    11/28 作業分

---
+++ src/ReadSurmo.tsx
@@ -0,0 +1,16 @@
+const surmoMeter :{[key: string]:string} = {};
+surmoMeter["f8:26:b7:72:bf:51"] = "キッチン";
+surmoMeter["f2:4c:0a:14:24:13"] = "リビング２";
+surmoMeter["f8:c2:38:8e:71:f6"] = "リビング１";
+surmoMeter["c2:9d:53:26:5f:d2"] = "寝室";
+
+export const ReadSurmo = () =>{
+
+    let test = surmoMeter["c2:9d:53:26:5f:d2"] 
+
+    return(
+        <div>
+            {test}
+        </div>
+    );
+};
---
+++ src/ShowSurmo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ShowSurmo = () =>{
+    return(
+    <>
+        テスト
+    </>
+    );
+};
+
+export default ShowSurmo
