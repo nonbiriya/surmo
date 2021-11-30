75e167083ab88b922a34a21e34282f35bdc85b38
nonbiriya <>
2021-11-30 23:25:31 +0900

    サーバー側もjavascriptで作成。

--- node/index.js
+++ node/index.js
@@ -1,9 +1,27 @@
+'use strict';
+
 const path = require("path");
 const express = require("express");
 const app = express();
 
+const fs = require('fs');
+var csvSync = require('csvsync'); // requiring sync module
+
 //ミドルウエアでstaticパスを追加（ただ、これだけだと直アクセスや無いpathだと動かない）
 app.use(express.static(path.join(__dirname, "..", "build")));
+
+app.get('/test',function(req,res){
+
+    const file = 'ondo.txt';
+    let data = fs.readFileSync(file);
+    let ondo_csv = csvSync.parse(data);
+    var ondo_json = {}
+    ondo_csv.forEach(surmoMeter => {
+        var new_data = {ondo : surmoMeter[1], shitsudo: surmoMeter[2]}
+        ondo_json[surmoMeter[0]] = new_data
+    });
+    res.json(ondo_json)
+});
 
 //これを追加（全てをindex.htmlにリダイレクト。いわゆるrewrite設定）
 app.use((req, res, next) => {
@@ -13,3 +31,4 @@ app.use((req, res, next) => {
 app.listen(3001, () => {
     console.log("server started on port 3001");
 });
+
